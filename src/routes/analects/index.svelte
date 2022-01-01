2c25e8e6e085e2df79f7af5c26f68ba3ca4303a1
silloi <silloi1993@gmail.com>
2022-01-01 22:16:46 +0900

    feature: add analects 1-20 data

---
+++ src/routes/analects/index.svelte
@@ -0,0 +1,24 @@
+<h1>論語</h1>
+
+<ol>
+	<li><a href="/analects/1">學而</a></li>
+	<li><a href="/analects/2">為政</a></li>
+	<li><a href="/analects/3">八佾</a></li>
+	<li><a href="/analects/4">里仁</a></li>
+	<li><a href="/analects/5">公冶長</a></li>
+	<li><a href="/analects/6">雍也</a></li>
+	<li><a href="/analects/7">述而</a></li>
+	<li><a href="/analects/8">泰伯</a></li>
+	<li><a href="/analects/9">子罕</a></li>
+	<li><a href="/analects/10">鄉黨</a></li>
+	<li><a href="/analects/11">先進</a></li>
+	<li><a href="/analects/12">顏淵</a></li>
+	<li><a href="/analects/13">子路</a></li>
+	<li><a href="/analects/14">憲問</a></li>
+	<li><a href="/analects/15">衛靈公</a></li>
+	<li><a href="/analects/16">季氏</a></li>
+	<li><a href="/analects/17">陽貨</a></li>
+	<li><a href="/analects/18">微子</a></li>
+	<li><a href="/analects/19">子張</a></li>
+	<li><a href="/analects/20">堯曰</a></li>
+</ol>
