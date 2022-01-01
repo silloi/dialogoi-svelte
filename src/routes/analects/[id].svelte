0b6ff308fcefdbb14c55e0cd1d86dd1f79e0d77a
silloi <silloi1993@gmail.com>
2022-01-01 17:28:26 +0900

    chore: 1 indent -> 2 space

--- src/routes/analects/[id].svelte
+++ src/routes/analects/[id].svelte
@@ -1,29 +1,29 @@
 <script context="module" lang="ts">
-	import type { Load } from '@sveltejs/kit';
+  import type { Load } from '@sveltejs/kit';
 
-	// see https://kit.svelte.dev/docs#loading
-	export const load: Load = async ({ params, fetch }) => {
-		const res = await fetch(`/data/${params.id}.txt`);
+  // see https://kit.svelte.dev/docs#loading
+  export const load: Load = async ({ params, fetch }) => {
+    const res = await fetch(`/data/${params.id}.txt`);
 
-		if (res.ok) {
-			const data = await res.text();
+    if (res.ok) {
+      const data = await res.text();
 
-			return {
-				props: { data }
-			};
-		}
+        return {
+          props: { data }
+        };
+    }
 
-		const { message } = await res.json();
+    const { message } = await res.json();
 
-		return {
-			error: new Error(message)
-		};
-	};
+    return {
+      error: new Error(message)
+    };
+  };
 </script>
 
 <script lang="ts">
-	export let name = 'gakuji';
-	export let data: string;
+  export let name = 'gakuji';
+  export let data: string;
 </script>
 
 <h1>Analects {name}</h1>
