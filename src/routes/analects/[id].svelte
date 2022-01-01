645926f53977939025fc7ac09a7747ff6e42d906
silloi <silloi1993@gmail.com>
2022-01-01 17:57:26 +0900

    chore: force space 4

--- src/routes/analects/[id].svelte
+++ src/routes/analects/[id].svelte
@@ -1,40 +1,40 @@
 <script context="module" lang="ts">
-  import type { Load } from '@sveltejs/kit';
-  import { parser } from '$lib/utils';
-  import { BOOK_TITLE } from '$lib/enum';
+	import type { Load } from '@sveltejs/kit';
+	import { parser } from '$lib/utils';
+	import { BOOK_TITLE } from '$lib/enum';
 
-  // see https://kit.svelte.dev/docs#loading
-  export const load: Load = async ({ params, fetch }) => {
-  const id = params.id;
-  const title = BOOK_TITLE[id];
+	// see https://kit.svelte.dev/docs#loading
+	export const load: Load = async ({ params, fetch }) => {
+		const id = params.id;
+		const title = BOOK_TITLE[id];
 
-  const res = await fetch(`/data/${id}.txt`);
+		const res = await fetch(`/data/${id}.txt`);
 
-  if (res.ok) {
-      const text = await res.text();
-      const data = parser(text.replace('\n', ''));
+		if (res.ok) {
+			const text = await res.text();
+			const data = parser(text.replace('\n', ''));
 
-      return {
-        props: {
-          id,
-          title,
-          data
-        }
-      };
-    }
+			return {
+				props: {
+					id,
+					title,
+					data
+				}
+			};
+		}
 
-    const { message } = await res.json();
+		const { message } = await res.json();
 
-    return {
-      error: new Error(message)
-    };
-  };
+		return {
+			error: new Error(message)
+		};
+	};
 </script>
 
 <script lang="ts">
-  export let id = 0;
-  export let title = '';
-  export let data: string;
+	export let id = 0;
+	export let title = '';
+	export let data: string;
 </script>
 
 <h1>Analects {id} {title}</h1>
